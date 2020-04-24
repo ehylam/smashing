b79b2f973cb62d71a879061ee1fc14a1428bf7a7
Eric Lam <ehylam.p@gmail.com>
2020-04-24 15:09:34 +1000

    Final commit

--- components/AppMasthead.vue
+++ components/AppMasthead.vue
@@ -1,9 +1,34 @@
 <template>
-  <img src="https://sakumastudio.com/blog/wp-content/uploads/sites/2/2020/03/IMG_6198.jpg" alt />
+  <img
+    src="https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    alt
+  />
 </template>
 
 <script>
-export default {};
+import { TimelineMax, TweenMax, Sine } from "gsap";
+
+export default {
+  methods: {
+    mastheadAnima() {
+      const tl = new TimelineMax();
+
+      tl.add("start");
+      tl.from(
+        "img",
+        2,
+        {
+          x: -600,
+          ease: Sine.easeOut
+        },
+        "start"
+      );
+    }
+  },
+  mounted() {
+    this.mastheadAnima();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
