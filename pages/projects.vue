2a401b37ef2b4ffbc401cb732d6a12ac50f78d9b
Eric Lam <ehylam.p@gmail.com>
2020-04-24 14:57:08 +1000

    Stage 4

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,12 +1,47 @@
 <template>
-  <div>
-    <h1>Projects</h1>
-  </div>
+  <main>
+    <section>
+      <h1>Projects</h1>
+      <div v-for="project in githubProjects" :key="project.id">
+        <h3>
+          <a :href="project.html_url">{{ project.name }}</a>
+        </h3>
+        <p>{{ project.stargazers_count }}</p>
+        <p>{{ project.homepage }}</p>
+        <p>{{ project.description }}</p>
+      </div>
+    </section>
+    <aside></aside>
+  </main>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["githubProjects"])
+  },
+  mounted() {
+    this.$store.dispatch("getGithubProjects");
+  }
+};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+main {
+  display: grid;
+  height: 100%;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  section {
+    grid-area: 1 / 3 / 6 / 6;
+    background-color: darkgrey;
+  }
+  aside {
+    grid-area: 1 / 1 / 6 / 3;
+  }
+}
 </style>
--- store/index.js
+++ store/index.js
@@ -16,9 +16,12 @@ export const actions = {
 			let githubProjects = await fetch(
 				`https://api.github.com/users/ehylam/repos?page=1&per_page=100`
 			).then(res => res.json());
+
+			githubProjects = githubProjects.filter(el => el.fork === false);
+			console.log(githubProjects);
+			commit('updateGithubProjects', githubProjects);
 		} catch (err) {
 			console.log(err);
 		}
-		commit('updateGitubProjects', payload);
 	}
 };
